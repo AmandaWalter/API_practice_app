46920e211f0f7a79734cfc0628d99c9604df2f71
Amanda Walter <anhart@gmail.com>
2015-02-27 00:50:14 -0700

    Set up Yelp API and display search results in a table

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    @parameters = { term: 'pub', limit: 20 }
+    @response = Yelp.client.search('Salt Lake City', @parameters, sort:2)
   end
 end
---
+++ config/initializers/yelp.rb
@@ -0,0 +1,8 @@
+require 'yelp'
+
+Yelp.client.configure do |config|
+  config.consumer_key = 'Qvl_qxcsBZiGBw_tGrCyZQ'
+  config.consumer_secret = 'GDXsCngL6gEoS9faILGBIA86oB0'
+  config.token = '-w2MF3OHM563DLZleMjUskoV1x7keF70'
+  config.token_secret = 'Eeb1OtNHxyz-PVcrbVCUwkOiRUM'
+end
